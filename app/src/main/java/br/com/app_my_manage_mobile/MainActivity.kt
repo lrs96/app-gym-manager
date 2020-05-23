e2eeaf9beb6e6646e87c6a85a03d51a9cda1f2d8
Luan Rodrigues <luanlrs96@gmail.com>
2020-05-22 23:15:43 -0300

    Maps e location

--- app/src/main/java/br/com/app_my_manage_mobile/MainActivity.kt
+++ app/src/main/java/br/com/app_my_manage_mobile/MainActivity.kt
@@ -69,6 +69,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 var i = Intent(this, FichaMedicaActivity::class.java)
                 startActivity(i)
             }
+            R.id.nav_localizacao -> {
+                startActivity(Intent(this, MapsActivity::class.java))
+            }
 
             R.id.nav_sair -> {
                 var editor: SharedPreferences.Editor =
